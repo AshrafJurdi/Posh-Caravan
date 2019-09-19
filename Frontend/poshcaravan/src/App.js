6e58cd61da7de9732b2ed153f1482e478643635f
Shirak <shirak.kevorkian@gmail.com>
2019-09-19 13:22:00 +0300

    fixed app js

--- Frontend/poshcaravan/src/App.js
+++ Frontend/poshcaravan/src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./App.css";
-import LandingSplash from "./components/LandingSplash/LandingSplash"
 
 function App() {
   return (
     <div className="App">
-      <LandingSplash/>
+      <p>a</p>
     </div>
   );
 }
